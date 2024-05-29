74ddd6f074754e3667d498ac65375f80aab68a73
Laiba Anwer <161801261+LaibaRind@users.noreply.github.com>
2024-05-29 04:27:01 -0700

    Delete Assignment 20 directory

--- Assignment 20/main.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-// Assignment # 19
-// Define array of oceans
-let World_oceans = ["Atlantic", "Pacific", "Indian", "Arctic"];
-// Print list of oceans
-console.log("list of oceans:", World_oceans.forEach(ocean => {
-    console.log(ocean);
-}));
