31221914293b09b94ba93a63be6c4b73bb5fcac5
Laiba Anwer <161801261+LaibaRind@users.noreply.github.com>
2024-05-29 04:28:25 -0700

    Delete Assignment 22 directory

--- Assignment 22/main.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-// Assignment # 21
-let intentionalError = ["laiba", "hani", "hadia", "sarah", "nida"];
-console.log(intentionalError[10]);
--- Assignment 22/main.ts
+++
@@ -1,8 +0,0 @@
-// Assignment # 22
-
-/*Intentional Error: If you haven’t received an array index error in one of your programs yet, 
-try to make one happen. Change an index in one of your programs to produce an index error. 
-Make sure you correct the error before closing the program.*/
-
-let intentionalError: string[] = ["laiba", "hani", "hadia","sarah","nida"];
-console.log(intentionalError[10]);
