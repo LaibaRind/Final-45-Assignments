2fee1db6269aa377207a1c39f7a5a7d8f9da5695
Laiba Anwer <161801261+LaibaRind@users.noreply.github.com>
2024-05-31 10:25:51 -0700

    Add files via upload

---
+++ Assignmnet 21/main.js
@@ -0,0 +1,22 @@
+"use strict";
+// Assignment # 21
+/*They think of something you could store in a TypeScript Object.
+Write a program that creates Objects containing these items*/
+let objectContainingitems = {
+    "iPhone": {
+        name: "iPhone",
+        price: 541999,
+        description: "Latest smartphone model"
+    },
+    "iPad": {
+        name: "iPad",
+        price: 341999,
+        description: "Latest tablet model"
+    },
+    "iPod": {
+        name: "iPod",
+        price: 241999,
+        description: "Latest music player"
+    }
+};
+console.log(objectContainingitems);
