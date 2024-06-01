97a1bc7198f100334885877d246d887ec68f9dcc
Laiba Anwer <161801261+LaibaRind@users.noreply.github.com>
2024-06-01 04:20:37 -0700

    Add files via upload

---
+++ Assignment 36/main.ts
@@ -0,0 +1,11 @@
+// Assignment # 36
+
+/*T-Shirt: Write a function called make_shirt() that accepts a size and the text of a message 
+that should be printed on the shirt. The function should print a sentence summarizing the size 
+of the shirt and the message printed on it. Call the function*/
+
+function make_shirt(size:string, message:string) {
+    console.log("I want a " + size + " size shirt with the message " + message);
+}
+
+make_shirt("midium","Palestine in my Heart");
