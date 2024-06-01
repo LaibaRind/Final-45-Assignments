e5c026fafc021a89f27de644d54ee6d85666add8
Laiba Anwer <161801261+LaibaRind@users.noreply.github.com>
2024-06-01 04:28:06 -0700

    Add files via upload

---
+++ Assignment 38/main.ts
@@ -0,0 +1,13 @@
+// Assignment # 38
+
+/*Cities: Write a function called describe_city() that accepts the name of a city and its country. 
+The function should print a simple sentence, such as Karachi is in Pakistan. 
+Give the parameter for the country a default value. Call your function for three different cities, 
+at least one of which is not in the default country.*/
+
+function describe_city(city:string, country:string = "Palestine") {
+    console.log(`${city} is the capital of ${country}`);   
+}
+describe_city("Jerusalem");
+describe_city("Gaza");
+describe_city("Khartoum", "Sudan");
