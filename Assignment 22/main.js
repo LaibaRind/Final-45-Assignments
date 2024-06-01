cf709beac84ae017a7f7c719cce5432df5bf6562
Laiba Anwer <161801261+LaibaRind@users.noreply.github.com>
2024-06-01 03:20:32 -0700

    Add files via upload

---
+++ Assignment 22/main.js
@@ -0,0 +1,7 @@
+"use strict";
+// Assignment # 22
+/*Intentional Error: If you haven’t received an array index error in one of your programs yet,
+try to make one happen. Change an index in one of your programs to produce an index error.
+Make sure you correct the error before closing the program.*/
+let intentionalError = ["laiba", "hani", "hadia", "sarah", "nida"];
+console.log(intentionalError[10]);
