ae6b4b90f71dd33a5274dcb5b17c05cd474a410b
Laiba Anwer <161801261+LaibaRind@users.noreply.github.com>
2024-06-01 03:33:36 -0700

    Add files via upload

---
+++ Assignment 25/main.js
@@ -0,0 +1,18 @@
+"use strict";
+// // Assignment # 25
+/* Imagine an alien was just shot down in a game.
+Create a variable called alien_color and assign it a value of 'green', 'yellow', or 'red'.*/
+/*• Write an if statement to test whether the alien’s color is green. If it is, print a message
+that the player just earned 5 points.*/
+let alien_color = "yellow";
+if (alien_color === "yellow") {
+    console.log("the player just earned 5 points.");
+}
+;
+/* • Write one version of this program that passes the if test and another that fails.
+(The version that fails will have no output.)*/
+alien_color = "green";
+if (alien_color === "yellow") {
+    console.log("Try again | no output.");
+}
+;
