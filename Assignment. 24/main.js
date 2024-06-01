840c751c6d4d73be643b6a2183cdd2314e6bd25c
Laiba Anwer <161801261+LaibaRind@users.noreply.github.com>
2024-06-01 03:25:32 -0700

    Add files via upload

---
+++ Assignment. 24/main.js
@@ -0,0 +1,49 @@
+"use strict";
+// Assignment 24
+/* More Conditional Tests: You don’t have to limit the number of tests you create to 10.
+If you want to try more comparisons, write more tests. Have at least one True and one False result
+for each of the following:*/
+// • Tests for equality and inequality with strings
+let password1 = "pasword234";
+let password2 = "pasword123";
+// Test for equality
+console.log(password1 == password2); //False
+// Test for inequality
+console.log(password1 != password2); //True
+// • Tests using the lower case function
+let UpperCase = "LAIBA";
+let lowercase = "laiba";
+console.log(UpperCase.toLowerCase() === lowercase); //True
+/* • Numerical tests involving equality and inequality, greater than and less than,
+greater than or equal to, and less than or equal to */
+let Num1 = 1923;
+let Num2 = 1324;
+// Test for equality
+console.log(Num1 === Num2); //False
+// Test for inequality
+console.log(Num1 != Num2); //True
+// Test for greater than
+console.log(Num1 > Num2); //True);
+// Test for less than
+console.log(Num1 < Num2); //False
+// Test for greater than or equal
+console.log(Num1 >= Num2); //True
+// Test for less than or equal
+console.log(Num1 < Num2); //False);
+// • Tests using "and" and "or" operators
+let prize1 = 320;
+let prize2 = 230;
+let prize3 = 100;
+let prize4 = 200;
+// Test for && operator
+console.log(prize1 != prize2 && prize3 < prize4); //True
+console.log(prize1 < prize2 && prize2 < prize3); //False
+// Test for || operator
+console.log(prize1 == prize2 || prize2 != prize3); //True
+console.log(prize1 < prize2 || prize2 < prize3); //False
+// • Test whether an item is in a array
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let itemtoFind = 5;
+console.log(array.indexOf(itemtoFind) !== -1); //True
+// • Test whether an item is not in a array 
+console.log(array.indexOf(11) == -1); //True
